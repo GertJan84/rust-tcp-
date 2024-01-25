8b485fede9ec5bfbfa5283d20a2d535c9123af10
Gert-Jan <gertjan@hackgreate.net>
2024-01-25 22:54:07 +0100

    Get groups and users

--- client/main.py
+++ client/main.py
@@ -1,8 +1,12 @@
 import asyncio
 import random
+import time
 
-usedNames = []
-channels = {} #hannel_name = [users]
+# TODO: Create new function that sends channels to scout tcp channel/group
+# HACK: Use regex instad of string manipulation
+
+usedNames = set() 
+channels = {} 
 
 async def get_users(channel):
     reader, writer = await asyncio.open_connection(
@@ -10,40 +14,42 @@ async def get_users(channel):
 
     # Get welcome
     await reader.read(1024)
-
-    #with open('names') as f:
-    #    names = f.readlines()
-    #    name = ''
-    #    while name and name not in usedNames:
-    #        name = random.choice(names)
-    #    name += '\n'
-
-    name = 'bot\n'
-
-    # send username
-    print(f'Connected as: {name!r}')
-    writer.write(name.encode())
-    await writer.drain()
-
-    await reader.read(1024)
-
-    print(f'Join {channel!r}')
-    channel += '\n'
-    writer.write(channel.encode())
-    await writer.drain()
-
-    await reader.read(1024)
-
-    writer.write(b'/users\n')
-    await writer.drain()
-
-    data = await reader.read(1024)
-    users = [user[2:] for user in data.decode().split('\n')[1:-1]]
-
-    channels[channel[:-1]] = users
-
-    print(channels)
     
+    with open('names') as f:
+        names = f.readlines()
+        names = [name.strip() for name in names]
+        availableNames = [name for name in names if name not in usedNames]
+
+        if availableNames:
+            name = random.choice(availableNames)
+            usedNames.add(name)
+
+            name += '\n'
+
+            # send username
+            writer.write(name.encode())
+            await writer.drain()
+
+            await reader.read(1024)
+
+            channel += '\n'
+            writer.write(channel.encode())
+            await writer.drain()
+
+            await reader.read(1024)
+
+            writer.write(b'/users\n')
+            await writer.drain()
+
+            data = await reader.read(1024)
+            users = [user[2:] for user in data.decode().split('\n')[1:-1]]
+            channels[channel[:-1]] = users
+
+            writer.write(b'/exit\n')
+            await writer.drain()
+
+            writer.write(b'\n')
+            await writer.drain()
 
 
 async def ask_channels():
@@ -52,40 +58,54 @@ async def ask_channels():
 
     # Get welcome
     data = await reader.read(1024)
-    print(f'{data.decode()}')
-
     message = 'bot\n'
 
     # send username
-    print(f'Send: {message!r}')
     writer.write(message.encode())
     await writer.drain()
 
-    ######### LOOP
+    await reader.read(1024)
 
-    # Get channels
-    data = await reader.read(1024)
-    print(f'Received: {data.decode()}')
+    while True:
+        writer.write(b'/groups\n')
+        await writer.drain()
 
-    # Send channel room
-    res_channels = [channel[2:] for channel in data.decode().split("\n")[3:-1]]
-    for channel in res_channels:
+        data = await reader.read(1024)
+        res_channels = [channel.split(" ")[1] for channel in data.decode().split("\n")[1:-1]] # get groups
 
-        print(channel)
+        for channel in res_channels:
+            if channel == 'None':
+                continue
 
-        if channel == 'None':
-            continue
+            channel = channel.split(" |")[0]
 
-        channel = channel.split(" |")[0]
+            channels[channel] = []
+            await get_users(channel)
 
-        if channel in channels:
-            continue
+        time.sleep(3)
 
-        channels[channel] = []
+        print(channels)
+        
+        # TODO: Fix this
+        # writer.write(b'scout\n')
+        # await writer.drain()
 
-        await get_users(channel)
+        # await reader.read(1024)
 
-    writer.write(b'/groups\n')
+        # for group, users in channels.items():
 
+            # channel_announce = f'**** {group} ****\n'
+            
+            # writer.write(channel_announce.encode())
+            # await writer.drain()
+
+            # for user in users:
+                # user += '\n'
+                # writer.write(user.encode())
+                # await writer.drain()
+
+
+        # writer.write(b'/disconnect\n')
+        # await writer.drain()
 
 asyncio.run(ask_channels())
